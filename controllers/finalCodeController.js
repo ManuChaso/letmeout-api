3a91987892c3b78bfff84953d7fa808a98283dd3
ManuChaso <manuc.chaso@gmail.com>
2024-03-19 19:15:48 +0100

    getFinalCode fixed

--- controllers/finalCodeController.js
+++ controllers/finalCodeController.js
@@ -4,19 +4,20 @@ function getFinalCode(req, res) {
   console.log(req.query.lobbyCode);
   console.log(req.query.id);
 
-  lobbyModel.findOne({ lobbyCode: req.query.lobbyCode }).then((lobbyFound) => {
-    console.log(lobbyFound);
-    lobbyFound.players
-      .forEach((player) => {
+  lobbyModel
+    .findOne({ lobbyCode: req.query.lobbyCode })
+    .then((lobbyFound) => {
+      console.log(lobbyFound);
+      lobbyFound.players.forEach((player) => {
         if (player.id == req.query.id) {
           res.status(200).send({ message: 'finalCode', finalCode: player.finalCode });
         }
-      })
-      .catch((err) => {
-        console.log('Error searching lobby', err);
-        res.status(500).send({ message: 'No se ha encontrado el lobby' });
       });
-  });
+    })
+    .catch((err) => {
+      console.log('Error searching lobby', err);
+      res.status(500).send({ message: 'No se ha encontrado el lobby' });
+    });
 }
 
 module.exports = { getFinalCode };
