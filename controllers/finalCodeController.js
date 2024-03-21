06d164a071a98ca60da92f4f5f50a87901f39f5f
ManuChaso <manuc.chaso@gmail.com>
2024-03-21 19:41:54 +0100

    fixed getFinalCode function: id parameter decoded

--- controllers/finalCodeController.js
+++ controllers/finalCodeController.js
@@ -1,18 +1,21 @@
 const lobbyModel = require('../models/lobby');
 
 function getFinalCode(req, res) {
-  console.log(req.query.lobbyCode);
-  console.log(req.query.id);
+  const id = decodeURIComponent(req.query.id);
+  const lobbyCode = req.query.lobbyCode;
+
+  console.log(lobbyCode);
+  console.log(id);
 
   lobbyModel
-    .findOne({ lobbyCode: req.query.lobbyCode })
+    .findOne({ lobbyCode: lobbyCode })
     .then((lobbyFound) => {
       console.log(lobbyFound);
       lobbyFound.players.forEach((player) => {
-        if (player.id == req.query.id) {
+        if (player.id == id) {
           res.status(200).send({ message: 'finalCode', finalCode: player.finalCode });
         } else {
-          res.status(200).send({ message: 'Wrong id' });
+          res.status(404).send({ message: 'Wrong id' });
         }
       });
     })
